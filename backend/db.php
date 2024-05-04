27747ed7d13ace4d5a49dd4d6a1a211d193ef14f
ansi0545 <ann-sofie.simonsson-haasz.0545@student.uu.se>
2024-05-04 21:21:53 +0200

    chore: Refactor database connection and user functions

--- backend/db.php
+++ backend/db.php
@@ -1,124 +1,82 @@
 <?php
-define("DB_SERVER", "127.0.0.1");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "vovvebloggen");
+$connection = mysqli_connect('127.0.0.1', 'root', '', 'vovvebloggen');
 
-$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-
-function add_user($username, $password)
-{
+function get_user($username) {
     global $connection;
-    $sql = 'INSERT INTO user (username, password) VALUES (?,?)';
-    $statement = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
-    mysqli_stmt_execute($statement);
-    mysqli_stmt_close($statement);
-}
-
-function get_result($statement)
-{
-    $rows = array();
-    $result = mysqli_stmt_get_result($statement);
-    if($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $rows[] = $row;
-        }
-    }
-    return $rows;
-}
-
-function get_user($username)
-{
-    global $connection;
-    $sql = 'SELECT * FROM user WHERE username=?';
-    $statement = mysqli_prepare($connection, $sql);
+    $statement = mysqli_prepare($connection, 'SELECT * FROM user WHERE username=?');
     mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
-    $result = get_result($statement);
+    $result = mysqli_stmt_get_result($statement);
     mysqli_stmt_close($statement);
-    return $result;
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function get_user_by_id($user_id) {
+function add_user($username, $password, $email) {
     global $connection;
-    $sql = 'SELECT * FROM user WHERE id=?';
-    $statement = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($statement, "i", $user_id);
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $statement = mysqli_prepare($connection, 'INSERT INTO user (username, password, email) VALUES (?, ?, ?)');
+    mysqli_stmt_bind_param($statement, "sss", $username, $password, $email);
     mysqli_stmt_execute($statement);
-    $result = get_result($statement);
     mysqli_stmt_close($statement);
-    return $result;
+}
+
+function change_avatar($avatar, $user_id) {
+    global $connection;
+    $statement = mysqli_prepare($connection, 'UPDATE user SET avatar=? WHERE id=?');
+    mysqli_stmt_bind_param($statement, "si", $avatar, $user_id);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);
 }
 
 function update_user_profile($user_id, $username, $email, $new_password = null) {
     global $connection;
-    // Update user's profile information and password if provided
+    $sql = 'UPDATE user SET username=?, email=?';
+    $params = [$username, $email];
+    if ($new_password !== null) {
+        $sql .= ', password=?';
+        $params[] = password_hash($new_password, PASSWORD_DEFAULT);
+    }
+    $sql .= ' WHERE id=?';
+    $params[] = $user_id;
+    $statement = mysqli_prepare($connection, $sql);
+    mysqli_stmt_bind_param($statement, str_repeat('s', count($params)), ...$params);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);
 }
 
 function upload_profile_picture($user_id, $file) {
-    // Upload user's profile picture to the server and save the filename in the database
+    // Assuming $file is an array with file info, as from $_FILES['fieldname']
+    $filename = $file['name'];
+    $filetmp = $file['tmp_name'];
+    $destination = "uploads/" . $filename;
+    move_uploaded_file($filetmp, $destination);
+    change_avatar($filename, $user_id);
 }
 
 function request_password_reset($email) {
-    // Send email with password reset link to the user's email address
+    global $connection;
+    $reset_code = bin2hex(random_bytes(20)); // Generate a random reset code
+    $sql = 'UPDATE user SET reset_code=? WHERE email=?';
+    $statement = mysqli_prepare($connection, $sql);
+    mysqli_stmt_bind_param($statement, "ss", $reset_code, $email);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);
+    // TODO: Send the reset code to the user's email address
 }
 
 function reset_password($email, $reset_code, $new_password) {
-    // Reset user's password in the database
-}
-
-function change_avatar($filename, $id)
-{
     global $connection;
-    $sql = 'UPDATE user SET image=? WHERE id=?';
+    $sql = 'SELECT * FROM user WHERE email=? AND reset_code=?';
     $statement = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($statement, "si", $filename, $id);
-    $result = mysqli_stmt_execute($statement);
+    mysqli_stmt_bind_param($statement, "ss", $email, $reset_code);
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
     mysqli_stmt_close($statement);
-    return $result;
-}
-
-function delete_post($id)
-{
-    global $connection;
-    $sql = 'DELETE FROM post WHERE id=?';
-    $statement = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($statement, "i", $id);
-    $result = mysqli_stmt_execute($statement);
-    mysqli_stmt_close($statement);
-    return $result;
-}
-
-function import($filename, $dropOldTables=FALSE)
-{
-    global $connection;
-    if ($dropOldTables) {
-        $query = 'SHOW TABLES';
-        $result = mysqli_query($connection, $query);
-        if ($result) {
-            while ($row = mysqli_fetch_row($result)) {
-                $query = 'DROP TABLE ' . $row[0];
-                if (mysqli_query($connection, $query))
-                    echo 'Table <strong>' . $row[0] . '</strong> dropped<br>';
-            }
-        }
+    if (count($result) === 1) {
+        $user_id = $result[0]['id'];
+        update_user_profile($user_id, $result[0]['username'], $email, $new_password);
+    } else {
+        // TODO: Handle the error case where the reset code is incorrect
     }
-    $query = '';
-    $lines = file($filename);
-    foreach ($lines as $line) {
-        if (substr($line, 0, 2) == '--' || $line == '')
-            continue;
-
-        $query .= $line;
-
-        if (substr(trim($line), -1, 1) == ';') {
-            if (!mysqli_query($connection, $query))
-                echo "<br>Error in query: <strong>$query</strong><br><br>";
-
-            $query = '';
-        }
-    }
-    echo 'Import finished!<br>';
 }
 ?>
